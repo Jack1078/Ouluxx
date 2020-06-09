e0cdbdf81694f2e89a0bf6e361f64568f5cc4ddd
sichael <mwangbox@gmail.com>
2020-06-09 14:40:55 -0400

    init store page

---
+++ src/layouts/StorePage/storePage_h.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './storePage_h.module.css'
+
+/**
+ * Layout for the Store page
+ */
+
+ const StorePage = () => {
+
+ };
+
+ export default StorePage;
