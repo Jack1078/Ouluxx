53e0c82d9b05b7dae92e5c8b3eb1eacc4953003c
sichael <mwangbox@gmail.com>
2020-07-05 11:52:25 -0400

    added back frontend

---
+++ client/src/layouts/SignupPage/signup_h.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import SignUpContainer from '../../containers/Signup/signup_k';
+import AboutContainer from '../../containers/About/about_k';
+import StoresNearbyContainer from '../../containers/StoresNearby/storesNearby_k';
+import Button from '../../components/button_c';
+import classes from './signup_h.module.css';
+import Footer from './../../containers/footer_k'
+
+/**
+ * Layout for the email sign up page
+ */
+
+function SignUp() {
+    return (
+        <div>
+            <section className={classes.background_image}>
+                <div className={classes.margin_left}>
+                    <Button
+                        className={classes.small_button}
+                        id="login_button"
+                        text="Log in"
+                        color="primary"
+                        fontColor="white"
+                    />
+                </div>
+                <SignUpContainer />
+                <StoresNearbyContainer />
+            </section>
+            <section className={[classes.background_white, classes.full_width].join(' ')}>
+                <AboutContainer />
+            </section>
+
+            <Footer />
+
+        </div>
+    );
+}
+
+export default SignUp;
---
+++ client/src/layouts/StoreSelectionPage/storeSelectionPage_h.jsx
@@ -0,0 +1,82 @@
+import React, {useState} from 'react';
+import StoreIcon from '../../components/storeIcon_c';
+import classes from './storeSelectionPage_h.module.css';
+import Filter from '../../containers/Filter/filter_k';
+import Logo from '../../images/logo.png';
+/**
+ * Layout for the Store Selection page
+ */
+
+const stores = [{name: "fairway",
+            categories: ["Groceries","Produce","Organic"],
+            img_url: "fairway.png"},
+            {name: "CVS Pharmacy@",
+            categories: ["Personal Care","Drugstore","Groceries"],
+            img_url: "cvs.png"},
+            {name: "HMart",
+            categories: ["Specialty","Prepared Meals","Ethnic"],
+            img_url: "hmart.png"},
+            {name: "Petco",
+            categories: ["Pet Supplies"],
+            img_url: "petco.png"},
+            {name: "ABC",
+            categories: ["Groceries","Produce","Organic"],
+            img_url: "fairway.png"},
+            {name: "DEF",
+            categories: ["Personal Care","Drugstore","Groceries"],
+            img_url: "cvs.png"},];
+
+const filters = ["All","Drugstore","Groceries","Pet Supplies", "Meals"];
+
+const filteredStores = (stores, filter) => {
+    return stores.filter(store => filter==="All"?true:store.categories.includes(filter))
+    .map(filtered_store => (
+        <StoreIcon
+        key={filtered_store.name}
+        name={filtered_store.name}
+        categories={filtered_store.categories}
+        img_url={filtered_store.img_url}
+        alt={filtered_store.name+" icon"}
+        onClick={()=>console.log('Store Icon clicked')}/>   
+    ))
+}
+
+const StoreSelect = () => {
+    const [active, setActive] = useState('All');
+    const [zipcode, setZipcode] = useState('11791');
+
+    return (
+        <div className={classes.background}>
+            <div className={classes.logo_container}>
+                <img src={Logo} alt='Ouluxx logo' height="45px"/>
+            </div>
+            <div className={classes.zipcode_container}>
+                Select Store for Delivery in&nbsp;<span style={{fontWeight: 'bold'}}>{zipcode}</span>
+            </div>
+            <div className={classes.filter_container}>
+                <Filter
+                    active={active}
+                    onChange={active=>setActive(active)}
+                >
+                    {filters.map((filter)=>{
+                        return(
+                            <div key={filter}>
+                                {filter}
+                            </div>
+                        );
+                    })}
+                </Filter> 
+            </div>
+            
+            <div className={classes.recommend_container}>
+                Recommend Stores
+            </div>
+            <div className={classes.stores_container}>
+                {filteredStores(stores, active)}
+            </div>
+        </div>
+    );
+
+}
+
+export default StoreSelect;
