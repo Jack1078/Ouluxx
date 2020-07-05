a0d2216533a3b75acf396d565737ea57a852dd96
sichael <mwangbox@gmail.com>
2020-07-05 11:42:24 -0400

    modified folder structure to include backend

---
+++ client/src/components/BackButton/backButton_c.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import classes from './backButton_c.module.css'
+
+/**
+ * Go back to the previous page
+ * @param {onClick} props 
+ */
+
+function BackButton(props) {
+    const {onClick} = props
+
+    return (
+        <button
+            type="button"
+            className={classes.BackArrowButton}
+            onClick={onClick}>
+            <svg
+                className={classes.BackArrowSVG}
+                viewBox="0 0 24 24">
+                <path
+                    d="M6 11.983c-.01.27.089.543.291.745l9 8.979a.999.999 
+                    0 0 0 1.415-.002c.39-.39.384-1.03 0-1.413L8.41 
+                    12.016l8.3-8.306a1.008 1.008 0 0 0 
+                    .001-1.416.998.998 0 0 0-1.415 0L6.29 
+                    11.308c-.185.185-.282.429-.29.676z"></path>
+            </svg>
+        </button>
+    );
+}
+
+BackButton.propTypes = {
+    onClick: PropTypes.func
+}
+
+export default BackButton;
---
+++ client/src/containers/Signup/signup_k.js
@@ -0,0 +1,104 @@
+/*
+Email sign up box container
+Contains: email signup textfield, sign up button,
+    continue with facebook button, continue with google button
+*/
+
+import React from 'react';
+import {createMuiTheme, ThemeProvider as MuiThemeProvider} from '@material-ui/core/styles';
+import Textfield from '../../components/textfield_c';
+import Button from '../../components/button_c';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import BackButton from '../../components/BackButton/backButton_c';
+import {FaGoogle} from 'react-icons/fa';
+import logo from '../../images/logo.png';
+import classes from './signup_k.module.css';
+
+/**
+ * Email sign up container
+ */
+
+const button_theme = createMuiTheme({
+    palette: {
+      primary: {main: '#3b5998'},
+      secondary: {main: '#4285F4'},
+    }
+})
+  
+function SignUp() {
+    return (
+        <div className={classes.container}>
+            {/* back to zip code page */}
+            <div>
+                <BackButton/>
+            </div>
+
+            {/* Email Sign up */}
+            <form className={classes.form}>
+
+                {/* Logo */}
+                <span className={classes.title}>
+                    <img
+                        className={classes.logo}
+                        src={logo}
+                        alt="a logo"/>
+                </span>
+
+                {/* Location changes based on zip code */}
+                <h1>Available in Syosset!</h1>
+                <h2>Create an account to start shopping</h2>
+
+                {/* Email textfield: enter email to sign up */}
+                <div className={classes.block}>
+                    <Textfield
+                        className={classes.textfield}
+                        id="email-textfield"
+                        label="Email"/>
+                </div>
+
+                {/* Email textfield: enter email to sign up */}
+                <div className={classes.block}>
+                    <p style={{textAlign: "center"}}>By signing up, you agree to our <a href=".">Terms of Service</a> & <a href=".">Privacy Policy</a></p>
+                </div>
+                <div className={classes.block}>
+                    <Button
+                        id="signup_button"
+                        text="Sign up with email"
+                        color="primary"
+                        fontColor="white"/>
+                </div>
+
+                <div className={classes.or_block}>
+                    <div className={classes.or}>or</div>
+                    <div className={classes.line}></div>
+                    <div className={classes.line} style={{right:"0"}}></div>
+                </div>
+
+                {/* Sign up with facebook or google account */}
+                <MuiThemeProvider theme={button_theme}>
+                    <div className={classes.block}>
+                        <Button
+                            id="facebook_signup_button"
+                            text="Continue with Facebook"
+                            color="primary"
+                            startIcon={<FacebookIcon/>}/>
+                    </div>
+                    <div className={classes.block}>
+                        <Button
+                            id="facebook_signup_button"
+                            text="Continue with Google"
+                            color="secondary"
+                            startIcon={<FaGoogle color="white"/>}/>
+                    </div>
+                </MuiThemeProvider>
+
+                {/* Log in option */}
+                <div className={classes.login}>
+                    Already have an account? <a href=".">Log in</a>
+                </div>
+            </form>
+        </div>
+    );
+}
+  
+export default SignUp;
