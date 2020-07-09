c5f9db3a3fd1aceeb582f40d7113242a26645a7b
kylluminati <2kenchill@gmail.com>
2020-07-09 14:32:26 -0400

    user cart functions added

--- Backend/Schema/user_schema.js
+++ Backend/Schema/user_schema.js
@@ -1,41 +1,41 @@
 /******************************************************************************
- * Name: Kyle Enchill														 *
- * Date: 7/2/2020															 *
- * Version: 1.0.0															 *
+ * Name: Kyle Enchill														  *
+ * Date: 7/2/2020															  *
+ * Version: 1.0.0															  *
  * Description: This file is the schema fo the user object. It contains useful*
- * information about the user such as name, address, email, cart, etc.		*
- * The cart information will be kept within the user as an array.
+ * information about the user such as name, address, email, cart, etc.		  *
+ * The cart information will be kept within the user as an array.             *
  ******************************************************************************/
 
 const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
-	Schema_Type : {type : String, default: "USER"}, // Identifies this as a store item. 
-	//UserID: String, // use the _id value
-	Username : String, 
-	Password : String, 
-	FirstName: String,
-	LastName: String,
-	Email: String,
-	Address: String,
-	City: String,
-	State: String,
-	Zipcode: Number,   //This does not verify if the zipcode is strictly XXXXX format
-	Cart: {
-		type: [{
-			ItemID: String, // uses the _id property of the item
-			ItemName: String,
-			Description: String,
-			Quantity: Number,
-			Price: Number,
-			Date_Ordered: {
-				type: Date,
-				default: Date.now
-			}
-		}],
-		default: []
-	}
+    Schema_Type: { type: String, default: "USER" }, // Identifies this as a store item. 
+    //UserID: String, // use the _id value
+    Username: String,
+    Password: String,
+    FirstName: String,
+    LastName: String,
+    Email: String,
+    Address: String,
+    City: String,
+    State: String,
+    Zipcode: Number,   //This does not verify if the zipcode is strictly XXXXX format
+    Cart: {
+        type: [{
+            ItemID: String, // uses the _id property of the item
+            ItemName: String,
+            Description: String,
+            Quantity: Number,
+            Price: Number,
+            Date_Ordered: {
+                type: Date,
+                default: Date.now
+            }
+        }],
+        default: []
+    }
 
 });
-	
+
 module.exports = User;
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,18 +1,22 @@
 /******************************************************************************
  * Name: Kyle Enchill														  *
- * Date: 7/3/2020															  *
- * Version: 1.0.0															  *
+ * Date: 7/5/2020															  *
+ * Version: 1.1.0															  *
  * Description: This file contains the functions for the users on our platform*
- * So far there is an add function to create new users in our database.		  *
+ * There are functions for the user and the user's cart. So far there is the  *
+ * basic create, retrieve, update, and delete for both items in the cart and  *
+ * users.																	  *
  ******************************************************************************/
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
 var user_schema = require('../Schema/user_schema');
+var Item_Schema = require('../Schema/Item_Schema');
 
 const url = 'mongodb://127.0.0.1:27017/Ouluxx'
 
 const UserModel = mongoose.model('UserAddModel', user_schema);
+const ItemModel = mongoose.model('ItemModel', Item_Schema);
 
 router.get('/', function (req, res, next) {
 	//this processes the GET request. 
@@ -25,6 +29,8 @@ router.get('/', function (req, res, next) {
 	console.log("Hello");
 });
 
+/************************** USER FUNCTIONS *************************************/
+//creates a new user in the database
 router.post('/add', async function (req, res, next) { // add a user to the db
 	//this processes the POST request. 
 	//this needs to be converted to sanitize the inputs.
@@ -41,7 +47,8 @@ router.post('/add', async function (req, res, next) { // add a user to the db
 		Address: req.body.Address,
 		City: req.body.City,
 		State: req.body.State,
-		Zipcode: req.body.Zipcode
+		Zipcode: req.body.Zipcode,
+		UserID: newuser._id
 
 	});
 
@@ -54,7 +61,171 @@ router.post('/add', async function (req, res, next) { // add a user to the db
 	mongoose.connection.close();
 });
 
-router.post('/testadd', function (req, res, next) {
+//retrieve user
+router.post('/get_user', async function (req, res, next) {
+	console.log(req.body);
+	mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+	await UserModel.findOne({ _id: mongoose.Types.ObjectId(req.body.userid) },
+		function (err, InventoryItemModel) {
+			res.json(JSON.stringify(UserModel))
+		});
+	mongoose.connection.close();
+});
+
+//updates user information
+router.post('/update', async function (req, res, next) {
+	console.log(req.body);
+	mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+	for (const [key, value] of Object.entries(req.body)) {
+		if (key.toString().toUpperCase().includes("ID") || key.toString().toUpperCase().includes("Name")) {
+			console.log(key); // cannot be changed
+		} else if (key.toString().toUpperCase() === "USERNAME") { //need to verify if the username is already taken
+			await UserModel.findOneAndUpdate(
+				{ _id: mongoose.Types.ObjectId(req.body.userid) },
+				{ "Username": value.toString() }
+			);
+		} else if (key.toString().toUpperCase() === "FIRSTNAME") {
+			await UserModel.findOneAndUpdate(
+				{ _id: mongoose.Types.ObjectId(req.body.userid) },
+				{ "FirstName": value.toString() }
+			);
+		} else if (key.toString().toUpperCase() === "LASTNAME") {
+			await UserModel.findOneAndUpdate(
+				{ _id: mongoose.Types.ObjectId(req.body.userid) },
+				{ "LastName": value.toString() }
+			);
+		} else if (key.toString().toUpperCase() === "ADDRESS") {
+			await UserModel.findOneAndUpdate(
+				{ _id: mongoose.Types.ObjectId(req.body.userid) },
+				{ "Address": value.toString() }
+			);
+		} else if (key.toString().toUpperCase() === "CITY") {
+			await UserModel.findOneAndUpdate(
+				{ _id: mongoose.Types.ObjectId(req.body.userid) },
+				{ "City": value.toString() }
+			);
+		} else if (key.toString().toUpperCase() === "STATE") {
+			await UserModel.findOneAndUpdate(
+				{ _id: mongoose.Types.ObjectId(req.body.userid) },
+				{ "State": value.toString() }
+			);
+		} else if (key.toString().toUpperCase() === "ZIPCODE") {
+			await UserModel.findOneAndUpdate(
+				{ _id: mongoose.Types.ObjectId(req.body.userid) },
+				{ "Zipcode": value.toString() }
+			);
+		}
+	}
+});
+
+//delete user from database
+router.post('/delete', async function (req, res, next) {
+	console.log(req.body);
+	mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+	await UserModel.findOneAndRemove({ _id: mongoose.Types.ObjectId(req.body.userid) });
+
+	var obj = new Object();
+	obj.status = "Success";
+	res.json(JSON.stringify(obj));
+
+	mongoose.connection.close();
+});
+
+/************************** CART FUNCTIONS *************************************/
+
+//adds an item to the user's cart
+router.post('/add_to_cart', async function (req, res, next) {
+	console.log(req, body);
+	mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+	var itemprice = parseFloat(req.body.itemprice);
+
+	var newItem = new ItemModel({
+		Name: req.body.itemname,
+		Price: itemprice,
+		StoreName: req.body.itemstore,
+		StoreID: req.body.storeid,
+		Category: req.body.categories,
+		Quantity: req.body.quantity
+	});
+
+	var CartItem = {
+		ItemID: newItem._id,
+		UserID: req.body.userid,
+		ItemName: req.body.itemname,
+		NumberInCart: req.body.number_in_cart
+	}
+
+	await UserModel.findOneAndUpdate(
+		{ _id: mongoose.Types.ObjectId(req.body.userid) },
+		{ $push: { Cart: CartItem } }
+	);
+
+	var obj = new Object();
+	obj.status = "Success";
+	res.json(JSON.stringify(obj));
+
+	mongoose.connection.close();
+
+});
+
+//retrieves the items in the user's cart
+router.post('get_cart', async function (req, res, next) {
+	console.log(req, body);
+	mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+	await UserModel.find({ UserID: req.body.userid, Cart: {} },
+		function (err, UserModel) {
+			res.json(JSON.stringify(UserModel))
+		});
+
+
+	mongoose.connection.close();
+});
+
+//updates the quantity of an object in the user's cart
+router.post('update_cart', async function (req, res, next) {
+	console.log(req, body);
+	mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+	await UserModel.findOneAndUpdate(
+		{
+			_id: mongoose.Types.ObjectId(req.body.userid),
+			Cart: { _id: mongoose.Types.ObjectId(req.body.ItemID) }
+		},
+		{ "Quantity": value.toString() }
+	);
+
+	var obj = new Object();
+	obj.status = "Success";
+	res.json(JSON.stringify(obj));
+	mongoose.connection.close();
+
+});
+
+//removes an item from the user's cart
+router.post('remove_from_cart', async function (req, res, next) {
+	console.log(req.body);
+	mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+	await UserModel.findOneAndUpdate(
+		{
+			_id: mongoose.Types.ObjectId(req.body.userid),
+			Cart: { _id: mongoose.Types.ObjectId(req.body.ItemID) }
+		},
+		{ $pull: { Cart: { ItemID: req.body.itemid } } }
+	);
+
+	var obj = new Object();
+	obj.status = "Success";
+	res.json(JSON.stringify(obj));
+	mongoose.connection.close();
+});
+
+
+/* router.post('/testadd', function (req, res, next) {
 	console.log(req.body);
 	mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 	var newuser = new UserModel({
@@ -69,13 +240,13 @@ router.post('/testadd', function (req, res, next) {
 
 	});
 
-	await newuser.save();
+await newuser.save();
 
-	var obj = new Object();
-	obj.hello = "World";
-	res.json(JSON.stringify(obj));
+var obj = new Object();
+obj.hello = "World";
+res.json(JSON.stringify(obj));
 
-	mongoose.connection.close();
-});
+mongoose.connection.close();
+}); */
 
 module.exports = router;
