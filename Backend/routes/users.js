4eb25ef51ef5f1143034c70721719b2d8e3d455f
Benjamin Salzberg <besrasal@gmail.com>
2020-07-11 23:19:03 -0400

    fixed errors

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -10,7 +10,6 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose'); 
-const UserModel = require('../Models/User_Model');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy; 
 const jwt = require("jsonwebtoken");
