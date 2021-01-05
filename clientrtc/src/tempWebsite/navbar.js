2262633874373e2fb32d18bd21e74a14c9a42da7
Benjamin Salzberg <besrasal@gmail.com>
2021-01-05 15:38:43 -0500

    Added firestore storage of stores
    Firebase firestore is now implemented for storing store data, allowing registration. The rules may need to be modified, but we do not have authorization for this, so there does not appear to be any actual danger regarding it (currently anyone can write, no one can read). The firebase firestore is still accessible by us, so we can see the data. It should be hostable now, using npm start, not firebase serve. This is because of react. This may make it difficult for us to run by hosting on google firebase hosting.

--- clientrtc/src/tempWebsite/navbar.js
+++ clientrtc/src/tempWebsite/navbar.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import classes from '../tempWebsite/navbar_style.module.css';
 import { Row, Col, Form, Button, Container, Navbar, FormControl, Modal, Dropdown } from 'react-bootstrap';
+import firebase from "firebase/app";
 
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+
+//const firebase = require("firebase");
+//require("firebase/firestore");
 
 function Nav() {
 
@@ -49,45 +56,45 @@ function Nav() {
                     </Container>
                     <br></br>
                     <Container>
-                        <Form>
+                        <Form onSubmit={mySubmitHandler}>
                             <Form.Row>
-                                <Form.Group as={Col} md="6" controlId="formBasicEmail">
+                                <Form.Group as={Col} md="6" controlId="Name">
                                     <Form.Label>Owner Name</Form.Label>
                                     <Form.Control type="text" placeholder="Enter full name" />
                                 </Form.Group>
-                                <Form.Group as={Col} md="6" controlId="formBasicEmail">
+                                <Form.Group as={Col} md="6" controlId="StoreName">
                                     <Form.Label>Store Name</Form.Label>
                                     <Form.Control type="text" placeholder="Enter store name" />
                                 </Form.Group>
                             </Form.Row>
 
                             <Form.Row>
-                                <Form.Group as={Col} md="4" controlId="formBasicEmail">
+                                <Form.Group as={Col} md="4" controlId="Email">
                                     <Form.Label>Email</Form.Label>
                                     <Form.Control type="text" placeholder="" />
                                 </Form.Group>
-                                <Form.Group as={Col} md="8" controlId="formBasicEmail">
+                                <Form.Group as={Col} md="8" controlId="WebURL">
                                     <Form.Label>Website URL</Form.Label>
                                     <Form.Control type="text" placeholder="" />
                                 </Form.Group>
                             </Form.Row>
 
                             <Form.Row>
-                                <Form.Group as={Col} md="6" controlId="validationCustom03">
+                                <Form.Group as={Col} md="6" controlId="City">
                                     <Form.Label>City</Form.Label>
                                     <Form.Control type="text" placeholder="City" required />
                                     <Form.Control.Feedback type="invalid">
                                         Please provide a valid city.
                                     </Form.Control.Feedback>
                                 </Form.Group>
-                                <Form.Group as={Col} md="3" controlId="validationCustom04">
+                                <Form.Group as={Col} md="3" controlId="State">
                                     <Form.Label>State</Form.Label>
                                     <Form.Control type="text" placeholder="State" required />
                                     <Form.Control.Feedback type="invalid">
                                         Please provide a valid state.
                                     </Form.Control.Feedback>
                                 </Form.Group>
-                                <Form.Group as={Col} md="3" controlId="validationCustom05">
+                                <Form.Group as={Col} md="3" controlId="Zip">
                                     <Form.Label>Zip</Form.Label>
                                     <Form.Control type="text" placeholder="Zip" required />
                                     <Form.Control.Feedback type="invalid">
@@ -96,7 +103,7 @@ function Nav() {
                                 </Form.Group>
                             </Form.Row>
                             <Form.Row>
-                                <Form.Group as={Col} md="12" controlId="exampleForm.ControlTextarea1" required>
+                                <Form.Group as={Col} md="12" controlId="StoreDescrip" required>
                                     <Form.Label>Store Description</Form.Label>
                                     <Form.Control as="textarea" rows={4} />
                                 </Form.Group>
@@ -109,7 +116,7 @@ function Nav() {
 
                             </Form.Row>
                             <br></br>
-                            <Button variant="warning" type="submit" onClick={handleClose} block>
+                            <Button variant="warning" type="submit" /*onClick={handleClose} */block>
                                 Register
                            </Button>
 
@@ -123,5 +130,68 @@ function Nav() {
         </>
     );
 }
+
+
+
+/*
+    initialize firebase
+*/
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyC1zsvjhH3lFvryJAxxlKnWMSQg2GXV0Ck",
+    authDomain: "ouluxxwebsiteregistration.firebaseapp.com",
+    projectId: "ouluxxwebsiteregistration",
+    storageBucket: "ouluxxwebsiteregistration.appspot.com",
+    messagingSenderId: "110377777379",
+    appId: "1:110377777379:web:c156afaf200bf3c5166dd6",
+    measurementId: "G-SWY6J6MZ5R"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+var db = firebase.firestore();
+
+
+
+/*
+    This is the function for handling the submit of the form. The submit of the form is done by pressing the button. 
+*/
+
+function mySubmitHandler(event){ 
+    /*
+        The parameter event is the form element from the above reactJS code.
+
+        On the submit of the form run this function. This function will interface with the form, and take the data and store it on the firebase firestore serverless DB. 
+        event.target is an array of the submitted form, to get the values use .value 
+    */
+    event.preventDefault();
+    var ownername = event.target[0].value;
+    var sname = event.target[1].value;
+    var email = event.target[2].value;
+    var weburl = event.target[3].value;
+    var city = event.target[4].value;
+    var state = event.target[5].value;
+    var zip = event.target[6].value;
+    var storedesc = event.target[7].value;
+    const StoreRegistration = db.collection("Stores").doc(sname);
+    StoreRegistration.set({
+            StoreName: sname,
+            URL: weburl,
+            Owners_Name: ownername, 
+            Email: email, 
+            City: city, 
+            State: state, 
+            Zip: zip, 
+            Store_Description: storedesc
+        })
+        .then(function() {
+            console.log("Document successfully written!");
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+  }
+
 
 export default Nav;
